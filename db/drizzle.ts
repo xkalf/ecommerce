51cf5bf76d9f21069c850dce5ce99e609694f3c3
bilguuntamir <mnymsuren37@gmail.com>
2024-10-01 08:42:26 +0800

    fix: drizzle adapter

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,8 +1,11 @@
-import * as schema from './schema'
+import * as schema from "./schema";
 
-import { drizzle } from 'drizzle-orm/vercel-postgres'
-import { sql } from '@vercel/postgres'
-const db = drizzle(sql, {
-  schema,
-})
-export default db
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+const client = postgres(process.env.POSTGRES_URL as string);
+const db = drizzle(client, {
+	schema,
+});
+
+export default db;
